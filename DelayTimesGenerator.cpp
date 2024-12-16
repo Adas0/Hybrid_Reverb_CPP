0b69db223cd578c55f858cbe7c07d72692350116
Adas0 <42966498+Adas0@users.noreply.github.com>
2024-12-16 17:03:13 +0100

    Update DelayTimesGenerator.cpp
    Code cleanup

--- DelayTimesGenerator.cpp
+++ DelayTimesGenerator.cpp
@@ -3,7 +3,7 @@
 
     DelayTimesGenerator.cpp
     Created: 16 Feb 2020 11:52:00am
-    Author:  Adam
+    Author:  Adam Korytowski
 
   ==============================================================================
 */
@@ -29,73 +29,7 @@ std::vector<int> DelayTimesGenerator::getDelayTimes(int & delayTimesNumber,
 		
 	}
 
-	/*for (int i = delayTimesNumber / 4; i < delayTimesNumber; ++i)
-	{
-		delayTimes.push_back(this->delayTimesPrime_[i]);
-
-	}*/
-
-	/*...*/
-
-	//for (int i = 0; i < delayTimesNumber / 2; ++i)
-	//{ 
-	//	//delayTimes.push_back(firstRefTime + generateRandomNumber(lowTime, highTime));
-	//	delayTimes.push_back(this->delayTimesPrime_[i * 2] + firstRefTime);
-	//	
-	//}
-
-	//for (int i = 3 * delayTimesNumber / 4; i < delayTimesNumber; ++i)
-	//{
-	//	//delayTimes.push_back(firstRefTime + generateRandomNumber(lowTime, highTime));
-	//	delayTimes.push_back(this->delayTimesPrime_[i * 2] + firstRefTime);
-	//	delayTimes.push_back(this->delayTimesPrime_[i * 2 + 1] + firstRefTime);
-
-	//}
-
-	//auto max = *max_element(std::begin(delayTimes), std::end(delayTimes));
-	//int a = 0;
-	//int b = 40;
-	//for (int i = 0; i < delayTimesNumber / 2; ++i)
-	//{
-	//	//delayTimes.push_back(delayTimes[i] + max);
-	//	delayTimes.push_back(firstRefTime + generateRandomNumber(a, b));
-
-	//}
-
-	/*
-	for (int i = 0; i < delayTimesNumber/4; ++i)
-	{
-		delayTimes.push_back(delayTimes[i] + max);
-
-	}
-
-	for (int i = 0; i < delayTimesNumber / 4; ++i)
-	{
-		delayTimes.push_back(delayTimes[i] + 2 *max);
-
-	}
-	for (int i = 0; i < delayTimesNumber / 4; ++i)
-	{
-		delayTimes.push_back(delayTimes[i] + 3 *max);
-
-	}*/
-	/*for (int i = 0; i < delayTimesNumber; ++i) 
-	{
-		delayTimes.push_back(this->delayTimesPrime_[4 * i]);
-
-	}*/
-
-	/*for (int i = 0; i < delayTimesNumber/3; ++i)
-	{
-		delayTimes.push_back((delayTimes[i] + (delayTimes[delayTimesNumber/3 - 1] )* 2));
-
-	}
-
-	for (int i = 0; i < delayTimesNumber / 3; ++i)
-	{
-		delayTimes.push_back((delayTimes[i] + (delayTimes[delayTimesNumber / 3 - 1]) * 3));
-
-	}*/
+	
 
 	std::sort(delayTimes.begin(), delayTimes.end());
 	std::reverse(delayTimes.begin(), delayTimes.end());
