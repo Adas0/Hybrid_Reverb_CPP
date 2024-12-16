258b48a95fd6031b8ea2910425821caa4fd29b16
Adas0 <42966498+Adas0@users.noreply.github.com>
2024-12-16 17:09:12 +0100

    Code cleanup

--- LateReverb.h
+++ LateReverb.h
@@ -3,7 +3,7 @@
 
     LateReverb.h
     Created: 29 Mar 2020 11:22:26am
-    Author:  Adam
+    Author:  Adam Korytowski
 
   ==============================================================================
 */
@@ -25,7 +25,6 @@ public:
 	int lateReverbNumLines;
 	AudioBuffer<float> createNoiseBufferArray(int& bufferSize);
 	AudioBuffer<float>noiseBuffer;
-	//std::vector<int> primeNumbers = { 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
 	std::vector<AudioBuffer<float>> noiseBuffers;
 
 };
