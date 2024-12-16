c4fae3a481bb5caed647450797139138946bae4a
Adas0 <42966498+Adas0@users.noreply.github.com>
2024-12-16 17:07:33 +0100

    Code cleanup

--- FilterGenerator.h
+++ FilterGenerator.h
@@ -3,7 +3,7 @@
 
     FiltersGenerator.h
     Created: 13 Mar 2020 6:18:42pm
-    Author:  Adam
+    Author:  Adam Korytowski
 
   ==============================================================================
 */
@@ -30,7 +30,6 @@ public:
 	dsp::ProcessorDuplicator<dsp::IIR::Filter<float>, dsp::IIR::Coefficients<float>> constantFiltersLeft[numberDelayLines];
 	dsp::ProcessorDuplicator<dsp::IIR::Filter<float>, dsp::IIR::Coefficients<float>> constantFiltersRight[numberDelayLines];
 
-//private:
 	std::vector<int>lowPassCutoffFrequenciesLeft;
 	std::vector<int>lowPassCutoffFrequenciesRight;
 	int filtersNumber;
@@ -43,6 +42,3 @@ public:
 	void prepareNoiseFilters(double sampleRate, int samplesPerBlock, int numChannels, dsp::ProcessSpec spec);
 
 };
-
-//w tej klasie mogą być wykonywane wszystkie operacje związane z tworzeniem filtrów. Tylko czy wtedy będzie łatwo przesłać te dane
-//do ewentualnej modyfikacji użytkownikowi w Editor - Tak.
